f4940313ff0e118bdc4e3c1ab0d0d87762dc57b1
postmannen <postmannen@gmail.com>
2024-01-30 23:04:13 +0100

    added nats pub sub to examples

---
+++ examples/nats pub sub/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	ac "github.com/postmannen/actress"
+)
+
+// Subject is put in Event.cmd[0], and the payload is in Event.Data
+const ETNatsClient ac.EventType = "ETNatsClient"
+const NatsSubject string = "mysubject"
+
+// Start both a nats subscriber and a nats publisher
+func etNatsClientFunc(ctx context.Context, p *ac.Process) func() {
+	fn := func() {
+		// Connect to the nats server
+		conn, err := nats.Connect("localhost:4222",
+			nats.MaxReconnects(-1),
+		)
+		if err != nil {
+			p.AddError(ac.Event{EventType: ac.ERFatal, Err: fmt.Errorf("error: nats.Connect failed, %vn", err)})
+
+		}
+		defer conn.Close()
+
+		// Subscribe for new incomming messages
+		go func() {
+			sub, err := conn.QueueSubscribe(NatsSubject, NatsSubject, func(msg *nats.Msg) {
+				p.AddEvent(ac.Event{
+					EventType: ac.ETPrint,
+					Data:      []byte(fmt.Sprintf("* nats: got message on subject %v, payload: %v", msg.Subject, string(msg.Data))),
+				})
+			})
+			if err != nil {
+				p.AddError(ac.Event{Err: fmt.Errorf("error: failed to nats.QueueSubscribe: %v", err)})
+			}
+			defer sub.Unsubscribe()
+			<-ctx.Done()
+
+		}()
+
+		// Publish messages. Subject is put in Event.cmd[0], and the payload is in Event.Data
+		for {
+			select {
+			case ev := <-p.InCh:
+				go func() {
+					//fmt.Printf("********* GOT EVENT: %v\n", ev)
+					err := conn.Publish(ev.Cmd[0], ev.Data)
+					if err != nil {
+						p.AddError(ac.Event{EventType: ac.ERFatal, Err: fmt.Errorf("error: failed to nats.Publish: %v", err)})
+					}
+				}()
+			case <-ctx.Done():
+				return
+			}
+		}
+
+	}
+
+	return fn
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Create a new root process.
+	rootAct := ac.NewRootProcess(ctx)
+	err := rootAct.Act()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Start up a Nats-server using the ETOsCmd EventType.
+	rootAct.AddEvent(ac.Event{
+		EventType: ac.ETOsCmd,
+		Cmd:       []string{"/bin/bash", "-c", "docker run --rm -p 4222:4222 -i nats:latest"},
+		NextEvent: &ac.Event{EventType: ac.ETPrint}})
+
+	// Wait a second so the nats-server is started before we connect the client.
+	time.Sleep(time.Second * 2)
+
+	// Create a nats client process.
+	err = ac.NewProcess(ctx, *rootAct, ETNatsClient, etNatsClientFunc).Act()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Loop and send Events to the new Nats Client process.
+	var nr int
+	for {
+		rootAct.AddEvent(ac.Event{
+			EventType: ETNatsClient,
+			Cmd:       []string{NatsSubject},
+			Data:      []byte(fmt.Sprintf("some data that was put in here: %v", nr)),
+		})
+		nr++
+		time.Sleep(time.Second * 2)
+	}
+}
--- go.mod
+++ go.mod
@@ -14,12 +14,17 @@ require (
 	github.com/felixge/fgprof v0.9.3 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/google/pprof v0.0.0-20211214055906-6f57359322fd // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
+	github.com/nats-io/nats.go v1.32.0 // indirect
+	github.com/nats-io/nkeys v0.4.7 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.45.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
